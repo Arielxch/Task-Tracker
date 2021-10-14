ea977e3f9b68b42eaa72af68f75228b98632b12b
Arielxch <xinccheng@gmail.com>
2021-10-13 23:13:02 -0400

    chore: remove footer button from about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Footer = () => {
-
+  // locate the current location by using useLocation from react-router-dom
+  const location = useLocation()
   return (
     <footer>
       <p>Copyright &copy;2021</p>
       <Link to="/about">
-        <Button
-          buttonColor='blue'
-          text='About' />
+        {/* only when the current page is home page, button is presented */}
+        {location.pathname === '/' && (
+          <Button
+            buttonColor='blue'
+            text='About' />
+        )}
       </Link>
 
     </footer>

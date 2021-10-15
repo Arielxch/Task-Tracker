2c932ac64c3f396fff7d51c5a872143ad06a4f85
Arielxch <xinccheng@gmail.com>
2021-10-14 21:40:47 -0400

    feat: add button id

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import { Link, useLocation } from 'react-router-dom'
-import Button from './Button'
+import { Link, useLocation } from 'react-router-dom';
+import Button from './Button';
 
 const Footer = () => {
   // locate the current location by using useLocation from react-router-dom
-  const location = useLocation()
+  const location = useLocation();
   return (
     <footer>
       <p>Copyright &copy;2021</p>
@@ -11,13 +11,14 @@ const Footer = () => {
         {/* only when the current page is home page, button is presented */}
         {location.pathname === '/' && (
           <Button
+            elementId='footer-button'
             buttonColor='blue'
             text='About' />
         )}
       </Link>
 
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

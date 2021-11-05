139ee54c77f4756f1d9962b47ed43fe02241c04a
Arielxch <xinccheng@gmail.com>
2021-11-05 00:19:12 -0400

    chore: add task redirect

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,19 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa';
+import { useHistory } from 'react-router';
 
 const Task = ({ task, onDelete, onToggle }) => {
+  const history = useHistory();
+  const onClickToRedirect = () => {
+    const path = `/task/${task.id}`;
+    history.push(path);
+  };
+
   return (
     <div
       className={`task ${task.reminder && 'reminder'}`}
-      onDoubleClick={() => onToggle(task.id)}>
+      onDoubleClick={() => onToggle(task.id)}
+      onClick={() => onClickToRedirect()}
+    >
       <h3>
         {task.text}{''}
         <FaTimes
@@ -15,7 +24,7 @@ const Task = ({ task, onDelete, onToggle }) => {
       </h3>
       <p>{task.day}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
